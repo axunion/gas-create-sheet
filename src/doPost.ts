deab9b2bf5a295559679d7ea2a87bfdf20c4b8d6
axunion <120321953+axunion@users.noreply.github.com>
2025-03-08 18:40:09 +0900

    Add response and remove unneeded code

--- src/doPost.ts
+++ src/doPost.ts
@@ -1,5 +1,6 @@
 type PostResponse = {
 	result: "done" | "error";
+	url?: string;
 	error?: string;
 };
 
@@ -12,10 +13,8 @@ function doPost(
 		const parameter = JSON.parse(e.postData.contents);
 		const type = parameter.type;
 		const name = parameter.name;
-		const email = parameter.email;
-		const recaptcha = parameter.recaptcha;
 
-		if (!type || !name || !email || !recaptcha) {
+		if (!type || !name) {
 			throw new Error("Invalid parameter.");
 		}
 
@@ -34,34 +33,17 @@ function doPost(
 			throw new Error("This form has expired.");
 		}
 
-		const recaptchaResult = verifyRecaptcha(secret, recaptcha);
-
-		if (!recaptchaResult.success || recaptchaResult.score < 0.5) {
-			const score = recaptchaResult.score || "-";
-			const error = recaptchaResult["error-codes"].join(" ");
-			throw new Error(`reCAPTCHA verification failed. ${score} ${error}`);
-		}
-
 		const copiedFile = duplicateTemplateFile({
 			spreadsheetId: config.spreadsheetId,
 			directoryId: config.directoryId,
-			name: name,
+			name,
 		});
 
 		if (!copiedFile) {
 			throw new Error("Failed to duplicate the template file.");
 		}
 
-		const subject =
-			"2025 JBBF全国青年フェローシップキャンプ 参加申込のお知らせ";
-		const body = `
-${name} 様
-お手続きありがとうございます。
-以下のファイルから申し込みをお願いします。
-
-${copiedFile.getUrl()}`;
-
-		GmailApp.sendEmail(email, subject, body);
+		response.url = copiedFile.getUrl();
 	} catch (error) {
 		response.result = "error";
 		response.error = error.message;
